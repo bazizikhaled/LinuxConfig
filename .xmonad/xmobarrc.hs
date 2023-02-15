4529b05a30c530e0df15f767e8791e597622939d
bazizikhaled <94703804+bazizikhaled@users.noreply.github.com>
2023-02-15 20:24:40 +0100

    Update xmobarrc.hs

--- .xmonad/xmobarrc.hs
+++ .xmonad/xmobarrc.hs
@@ -1,38 +1,13 @@
 Config {
-       font = "xft:Zekton:size=18:bold:antialias=true"
-       , additionalFonts = [ "xft:FontAwesome:size=11" ]
+       font = "xft:Zekton:size=19:bold:antialias=true"
+       , additionalFonts = [ "xft:FontAwesome:size=12" ]
        , allDesktops = True
        , bgColor = "#282c34"
        , fgColor = "#bbc2cf"
-       , position = TopW L 100
-       , commands = [ Run Cpu [ "--template", "<total>%"
-                              , "--Low","3"
-                              , "--High","50"
-                              , "--low","#bbc2cf"
-                              , "--normal","#bbc2cf"
-                              , "--high","#fb4934"] 50
+       , position = TopW L 110
+       , commands = [
+                     Run Date "%a %b %_d %I:%M" "date" 300
 
-                    , Run Memory ["-t","<usedratio>%"
-                                 ,"-H","80"
-                                 ,"-L","10"
-                                 ,"-l","#bbc2cf"
-                                 ,"-n","#bbc2cf"
-                                 ,"-h","#fb4934"] 50
-
-                    , Run Date "%a %b %_d %I:%M" "date" 300
-                    , Run DynNetwork ["-t"," <rx>,  <tx>"
-                                     ,"-H","200"
-                                     ,"-L","10"
-                                     ,"-h","#bbc2cf"
-                                     ,"-l","#bbc2cf"
-                                     ,"-n","#bbc2cf"] 50
-
-                    , Run CoreTemp ["-t", " <core0>°"
-                                   , "-L", "30"
-                                   , "-H", "75"
-                                   , "-l", "lightblue"
-                                   , "-n", "#bbc2cf"
-                                   , "-h", "#aa4450"] 50
 		    , Run Uptime ["-p", "uptime: <days>d <hours>h"] 50
                     -- battery monitor
                     , Run BatteryP       [ "BAT0" ]
@@ -55,6 +30,7 @@ Config {
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = " %StdinReader% }{ %uptime% | %cpu% | %coretemp% | %memory% | %battery% | %dynnetwork% | %date%  |"   -- #69DFFA
+       , template = " %StdinReader% }{  %uptime% |  %battery% | %date%  |"   -- #69DFFA
+
        }
 
